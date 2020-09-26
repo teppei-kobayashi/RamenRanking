d793e3ce88319ec9569f6340bea4ca6c2f5c36b8
teppei.kobayashi <teppe0527@gmail.com>
2020-09-26 23:11:21 +0900

    Add Ramen edit

---
+++ src/controllers/ramen/RamensEditServlet.java
@@ -0,0 +1,49 @@
+package controllers.ramen;
+
+import java.io.IOException;
+
+import javax.persistence.EntityManager;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import models.Ramen;
+import utils.DBUtil;
+
+/**
+ * Servlet implementation class RamensEditServlet
+ */
+@WebServlet("/ramens/edit")
+public class RamensEditServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public RamensEditServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        EntityManager em = DBUtil.createEntityManager();
+
+        Ramen r = em.find(Ramen.class, Integer.parseInt(request.getParameter("id")));
+
+        em.close();
+
+        request.setAttribute("ramen", r);
+        request.setAttribute("_token", request.getSession().getId());
+        request.getSession().setAttribute("ramen_id", r.getId());
+
+        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/ramens/edit.jsp");
+        rd.forward(request, response);
+    }
+
+}
